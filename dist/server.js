d49d85cf4c75d2c26c3f177a1adc8a70c44df5f1
Belal-Aljumaa <bilal.joma90@gmail.com>
2022-12-14 01:28:15 +0100

    static web server

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import path from 'path';
 const app = express();
 const port = process.env.PORT || 3499;
+const __dirname = path.resolve(path.dirname(''));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './public/views'));
 app.get('/', (req, res) => {
-    res.send(`
-        <p>this is <b>HTML</b></p>
-    `);
+    req.res.send(`
+      <h1>Home</h1>
+      <p>go to the <a href="/info">info page</a></p>
+  `);
+});
+app.get('/info', (req, res) => {
+    req.res.send(`
+      <h1>Info</h1>
+      <p>go back <a href="/">home</a></p>
+  `);
 });
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,28 @@
 import express from 'express';
+import path from 'path';
+
 
 const app = express();
 const port = process.env.PORT || 3499;
+const __dirname = path.resolve(path.dirname(''));
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './public/views'));
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send(`
-        <p>this is <b>HTML</b></p>
-    `);
+  req.res.send(`
+      <h1>Home</h1>
+      <p>go to the <a href="/info">info page</a></p>
+  `);
 });
 
+app.get('/info', (req: express.Request, res: express.Response) => {
+  req.res.send(`
+      <h1>Info</h1>
+      <p>go back <a href="/">home</a></p>
+  `);
+});
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
 });
