f1c760053e06cea0afa1c312b02e0bb5ffcddc9b
Belal-Aljumaa <bilal.joma90@gmail.com>
2022-12-14 01:55:04 +0100

    implemented static pugjs

--- dist/server.js
+++ dist/server.js
@@ -7,16 +7,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './public/views'));
 app.get('/', (req, res) => {
-    req.res.send(`
-      <h1>Home</h1>
-      <p>go to the <a href="/info">info page</a></p>
-  `);
+    res.render('index');
 });
 app.get('/info', (req, res) => {
-    req.res.send(`
-      <h1>Info</h1>
-      <p>go back <a href="/">home</a></p>
-  `);
+    res.render('info');
 });
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import path from 'path';
 
-
 const app = express();
 const port = process.env.PORT || 3499;
 const __dirname = path.resolve(path.dirname(''));
@@ -11,17 +10,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './public/views'));
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  req.res.send(`
-      <h1>Home</h1>
-      <p>go to the <a href="/info">info page</a></p>
-  `);
+  res.render('index');
 });
 
 app.get('/info', (req: express.Request, res: express.Response) => {
-  req.res.send(`
-      <h1>Info</h1>
-      <p>go back <a href="/">home</a></p>
-  `);
+  res.render('info');
 });
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
